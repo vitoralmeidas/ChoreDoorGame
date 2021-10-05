716b9a76f63985d443a3e54eec809b2de9c4a4d6
Vitor Almeida <vitor.oliveira1904@gmail.com>
2021-10-04 21:12:38 -0300

    adding img to the doors

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 let doorImage1 = document.getElementById("door1");
-let botDoorPath = 'https://content.codecademy.com/projects/chore-door/images/robot.svg'
+let doorImage2 = document.getElementById("door2");
+let doorImage3 = document.getElementById("door3");
+let botDoorPath = 'https://content.codecademy.com/projects/chore-door/images/robot.svg';
+let beachDoorPath = "https://content.codecademy.com/projects/chore-door/images/beach.svg";
+let spaceDoorPath = "https://content.codecademy.com/projects/chore-door/images/space.svg";
+
 
 // change the door image to bot image
 doorImage1.onclick = () => {
     doorImage1.src = botDoorPath;
 }
 
+
+doorImage2.onclick = () => {
+
+}
+
+doorImage3.onclick = () => {
+
+}
